a29a940c56728fdf789a825c0c33f26218b17647
mrawalofc <mrawalyt74@gmail.com>
2026-04-30 23:18:54 +0300

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'love-world-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/src/main.tsx',
+  '/src/App.tsx',
+  '/src/index.css',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).catch(() => {
+        if (event.request.mode === 'navigate') {
+          return caches.match('/index.html');
+        }
+      });
+    })
+  );
+});
